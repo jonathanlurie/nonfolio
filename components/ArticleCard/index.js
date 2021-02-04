4f28f6f4c784cf05e525cb4c9c391e3defbfff32
jonathanlurie <lurie.jo@gmail.com>
2021-02-04 23:10:01 +0100

    ENH article card

--- components/ArticleCard/index.js
+++ components/ArticleCard/index.js
@@ -1,5 +1,11 @@
 import React from 'react'
 import Image from 'next/image'
+import {
+  Row,
+  Col,
+  Space,
+} from 'antd'
+import { parseISO, format } from 'date-fns'
 import Styles from './styles.module.css'
 
 export default class ArticleCard extends React.Component {
@@ -11,13 +17,35 @@ export default class ArticleCard extends React.Component {
 
   render() {
     const article = this.props.article
+    const niceDate = article.date ? format(parseISO(article.date), 'LLLL d, yyyy') : null
 
     return (
       <div className={Styles['container']}>
+
         <div className={Styles['text-container']}>
-          <div className={Styles['title']}>
-            {article.title}
-          </div>
+        <Row justify="space-around" align="middle" style={{height: '100%', width: '100%'}}>
+            <Col span={24}>
+              
+              <div className={Styles['title']}>
+                {article.title}
+              </div>
+              <div className={Styles['date']}>
+                {niceDate}
+              </div>
+            </Col>
+          </Row>
+        </div>
+
+        <div className={Styles['excerpt']}>
+          {article.excerpt}
+        </div>
+
+        <div className={Styles['tag-container']}>
+            <Space>
+              {
+                article.tags.map((t, i) => <div className={Styles['tag']} key={i}>{t}</div>)
+              }
+            </Space>
         </div>
 
         <div className={Styles['dark-background']}/>
