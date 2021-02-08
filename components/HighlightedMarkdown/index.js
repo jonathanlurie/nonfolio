3a1bbde30e2c6870b3bda0697e05848c0f5aac86
jonathanlurie <jonathan.lurie@epfl.ch>
2021-02-08 11:14:39 +0100

    ADD lazy loading for img from articles

--- components/HighlightedMarkdown/index.js
+++ components/HighlightedMarkdown/index.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import Image from 'next/image'
 import Markdown from 'markdown-to-jsx'
 import hljs from 'highlight.js'
 import 'highlight.js/styles/github.css'
 
+
+const LazyImg = ({ children, ...props }) => (
+  <img loading='lazy' {...props}>{children}</img>
+)
 
 export default class HighlightedMarkdown extends React.Component {
   constructor(props) {
@@ -17,13 +22,22 @@ export default class HighlightedMarkdown extends React.Component {
     nodes.forEach((node) => {
       hljs.highlightBlock(node)
     })
-    
   }
 
   render() {
     return (
       <div ref={this._htmlDivRef}>
-        <Markdown>{this.props.children}</Markdown>
+        <Markdown
+          options={{
+            overrides: {
+                img: {
+                    component: LazyImg,
+                },
+            },
+          }}
+        >
+          {this.props.children}
+        </Markdown>
       </div>
     );
   }
